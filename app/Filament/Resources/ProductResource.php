296ea053e6ae7e8a571a1240808b6f96e5b2314a
Jurairat11 <141710195+Jurairat11@users.noreply.github.com>
2025-04-02 15:45:00 +0700

    nav

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -17,7 +17,9 @@ class ProductResource extends Resource
 {
     protected static ?string $model = Product::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-lifebuoy';
+
+    protected static ?string $navigationGroup = 'Product Management';
 
     public static function form(Form $form): Form
     {
